3a1f651e5f7c51795631452dfebc7f375d127008
Arisa <uliannaobr@gmail.com>
2023-04-21 13:47:12 +0400

    refactor lab-2

--- lab-1/lab-1.py
+++ lab-1/lab-1.py
@@ -7,21 +7,22 @@ from skimage.io import imread, imsave, imshow, show
 from matplotlib import pyplot as plt
 from skimage import data, exposure  # , img_as_float
 
-path = 'images\\test.jpg'
+path = '../images\\test.jpg'
 image = imread(path)
 g_min = 0
 g_max = 255
 
 
 def add_plot(fig, rows, columns, cell, x, y, _title):
-    plt.rcParams['font.size'] = '7'
+    plt.rcParams['font.size'] = '6'
     fig.add_subplot(rows, columns, cell)
     title(_title)
+
     plt.plot(x, y, color='teal', linewidth=2)
 
 
 def add_picture(fig, rows, columns, cell, image, _title):
-    plt.rcParams['font.size'] = '7'
+    plt.rcParams['font.size'] = '6'
     fig.add_subplot(rows, columns, cell)
     title(_title)
     imshow(image)
@@ -29,32 +30,31 @@ def add_picture(fig, rows, columns, cell, image, _title):
 
 def threshold_processing():
     fig = plt.figure(figsize=(12, 5))
-    add_picture(fig, 2, 3, 1, image, 'The original image')
+    add_picture(fig, 2, 3, 1, image, 'Исходное изображение')
     hist, bins = histogram(image)
-    add_plot(fig, 2, 3, 2, bins, hist, 'The original histogram')
+    add_plot(fig, 2, 3, 2, bins, hist, 'Исходная гистограмма')
 
-    # threshold(порог) for image
     threshold = 125
     threshold_processing_image = image > threshold
     threshold_processing_image = (threshold_processing_image * 255).astype(np.uint8)
 
-    add_picture(fig, 2, 3, 4, threshold_processing_image, 'The image after threshold processing')
+    add_picture(fig, 2, 3, 4, threshold_processing_image, 'Изображение после пороговой обработки')
     hist, bins = histogram(threshold_processing_image)
-    add_plot(fig, 2, 3, 5, bins, hist, 'The histogram after threshold processing')
+    add_plot(fig, 2, 3, 5, bins, hist, 'Гистограмма после пороговой обработки')
 
     x = np.sort(image.ravel())
     y = np.sort(threshold_processing_image.ravel())
-    add_plot(fig, 2, 3, 6, x, y, 'The function element - by - element conversion')
+    add_plot(fig, 2, 3, 6, x, y, 'График функции поэлементного преобразования')
 
     show()
 
 
 def contrasting():
     fig = plt.figure(figsize=(12, 5))
-    add_picture(fig, 2, 3, 1, image, 'The original image')
+    add_picture(fig, 2, 3, 1, image, 'Исходное изображение')
 
     hist, bins = histogram(image)
-    add_plot(fig, 2, 3, 2, bins, hist, 'The original histogram')
+    add_plot(fig, 2, 3, 2, bins, hist, 'Исходная гистограмма')
 
     # [fmin,fmax] - real range
     f_min = image.min()
@@ -64,23 +64,21 @@ def contrasting():
     b = (g_min * f_max - g_max * f_min) / (f_max - f_min)
 
     contrasted_image = (a * image + b).astype(np.uint8)
-    add_picture(fig, 2, 3, 4, contrasted_image, 'The contrasted image')
+    add_picture(fig, 2, 3, 4, contrasted_image, 'Контрастированное изображение')
 
     hist, bins = histogram(contrasted_image)
-    add_plot(fig, 2, 3, 5, bins, hist, 'The histogram after contrasting processing')
+    add_plot(fig, 2, 3, 5, bins, hist, 'Гистограмма после контрастирования')
 
     x = np.sort(image.ravel())
     y = np.sort(contrasted_image.ravel())
-    title('The function element - by - element conversion')
-    add_plot(fig, 2, 3, 6, x, y, 'The function element - by - element conversion')
+    add_plot(fig, 2, 3, 6, x, y, 'График функции поэлементного преобразования')
 
     show()
 
 
-# 3) Equalization
 def equalization():
     fig = plt.figure(figsize=(17, 7))
-    add_picture(fig, 3, 4, 1, image, 'The original image')
+    add_picture(fig, 3, 4, 1, image, 'Исходное изображение')
 
     hist, bins = np.histogram(image.flatten(), 256, [0, 256])
     cumsum = hist.cumsum()
@@ -88,31 +86,30 @@ def equalization():
     g = (g_max - g_min) * F + g_min
     equalization_image = g[image]
     equalization_image = equalization_image.astype(np.uint8)
-    # self-written = самописной
-    add_picture(fig, 3, 4, 2, equalization_image, 'The self-written equalization image')
+    add_picture(fig, 3, 4, 2, equalization_image, 'Изображение после эквализации (самописной)')
 
     standart_equalization_image = exposure.equalize_hist(image)
-    add_picture(fig, 3, 4, 3, standart_equalization_image, 'Image after standart equalization')
+    add_picture(fig, 3, 4, 3, standart_equalization_image, 'Изображение после эквализации (стандратной)')
 
     hist, bins = histogram(image)
-    add_plot(fig, 3, 4, 5, bins, hist, 'The histogram of original image')
+    add_plot(fig, 3, 4, 5, bins, hist, 'Гистограмма исходного изображения')
 
     x = np.arange(0, 256, 1)
-    add_plot(fig, 3, 4, 6, x, F, 'Graph of the cumulative distribution function of brightness before')
+    add_plot(fig, 3, 4, 6, x, F, 'График интегральной функции распределения яркости (до обработки)')
 
     hist, bins = np.histogram(equalization_image.flatten(), 256, [0, 256])
     cumsum1 = hist.cumsum()
     F_after_equalization = (cumsum1 - cumsum1.min()) / (cumsum1.max() - cumsum1.min())
     g_after_equalization = (g_max - g_min) * F + g_min
-    add_plot(fig, 3, 4, 7, x, F_after_equalization, 'Graph of the cumulative distribution function of brightness after')
+    add_plot(fig, 3, 4, 7, x, F_after_equalization, 'График интегральной функции распределения яркости (после обработки)')
 
-    add_plot(fig, 3, 4, 8, x, g[x], 'The function element - by - element conversion')
+    add_plot(fig, 3, 4, 8, x, g[x], 'График функции поэлементного преобразования')
 
     hist, bins = histogram(equalization_image)
-    add_plot(fig, 3, 4, 9, bins, hist, 'The histogram of self-written equalization image')
+    add_plot(fig, 3, 4, 9, bins, hist, 'Гистограмма изображения после эквализации (самописной)')
 
     hist, bins = histogram(standart_equalization_image)
-    add_plot(fig, 3, 4, 10, bins, hist, 'The histogram of standart equalization image')
+    add_plot(fig, 3, 4, 10, bins, hist, 'Гистограмма изображения после эквализации (стандартной)')
 
     plt.tight_layout()
     show()
@@ -120,9 +117,9 @@ def equalization():
 
 if __name__ == '__main__':
     threshold_processing()
-    # contrasting()
-    # equalization()
+    contrasting()
+    equalization()
     hist, bins = np.histogram(image.flatten(), 256, [0, 256])
     print(bins)
-    print('\n')
-    print(hist)
+    # print('\n')
+    # print(hist)
---
+++ lab-2/lab-2.py
@@ -0,0 +1,199 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage.io import imread, imshow
+from skimage.exposure import histogram
+from scipy import signal
+
+
+def thresholding(image, threshold):
+    thresholding_result = image > threshold
+    thresholding_result = (thresholding_result * 255)
+    return thresholding_result
+
+
+def simple_gradient(img):
+    vertical = np.array([[-1], [1]])
+    horizontal = np.array([[-1, 1]])
+    df_dx = signal.convolve2d(img, vertical, mode='same', boundary='symm')
+    df_dy = signal.convolve2d(img, horizontal, mode='same', boundary='symm')
+    result = np.sqrt(df_dx ** 2 + df_dy ** 2)
+    return df_dx, df_dy, result
+
+
+def laplacian_approximation(img, mask):
+    laplacian = signal.convolve2d(img, mask, mode='same', boundary='symm')
+    laplacian_estimation = np.abs(laplacian)
+    return laplacian_estimation
+
+
+def pruitt_operator(img):
+    mask1 = 1 / 6 * np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+    mask2 = 1 / 6 * np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+
+    s1 = signal.convolve2d(img, mask1, mode='same', boundary='symm')
+    s2 = signal.convolve2d(img, mask2, mode='same', boundary='symm')
+
+    e = np.sqrt(s1 ** 2 + s2 ** 2)
+    return s1, s2, e
+
+
+def agreement_laplacian(img):
+    mask = 1 / 3 * np.array([[2, -1, 2], [-1, -4, -1], [2, -1, 2]])
+    s = signal.convolve2d(img, mask, mode='same', boundary='symm')
+    e = np.abs(s)
+    return e
+
+
+if __name__ == '__main__':
+    path = '../images//07_elaine.tif'
+    image = imread(path)
+    u, v, gradient = simple_gradient(image)
+    simple_gradient_image = thresholding(gradient, 35)
+
+    fig = plt.figure(figsize=(12, 7))
+    plt.rcParams['font.size'] = '6'
+    fig.suptitle('Метод градиента')
+
+    fig.add_subplot(2, 3, 1)
+    plt.title('Исходное изображение')
+    imshow(image, cmap='gray')
+
+    fig.add_subplot(2, 3, 2)
+    plt.title('Частная производная по первому направлению')
+    imshow(u + 128, cmap='gray')
+
+    fig.add_subplot(2, 3, 3)
+    plt.title('Частная производная по второму направлению')
+    imshow(v + 128, cmap='gray')
+
+    fig.add_subplot(2, 3, 4)
+    plt.title('Оценка модуля градиента')
+    imshow(gradient, cmap='gray')
+
+    fig.add_subplot(2, 3, 5)
+    hist, bins = histogram(gradient)
+    plt.title('Гистограмма оценки модуля градиента')
+    plt.plot(bins, hist)
+
+    fig.add_subplot(2, 3, 6)
+    plt.title('Контуры метода простого градиента')
+    imshow(simple_gradient_image, cmap='gray')
+
+    plt.show()
+
+    ################################################################
+    mask1 = np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
+    mask2 = 1 / 2 * np.array([[1, 0, 1], [0, -4, 0], [1, 0, 1]])
+    mask3 = 1 / 3 * np.array([[1, 1, 1], [1, -8, 1], [1, 1, 1]])
+
+    laplasian1 = laplacian_approximation(image, mask1)
+    laplasian2 = laplacian_approximation(image, mask2)
+    laplasian3 = laplacian_approximation(image, mask3)
+
+    fig = plt.figure(figsize=(12, 7))
+    fig.suptitle('Метод проксимации лапласиана')
+    fig.add_subplot(3, 4, 1)
+    plt.title('Исходное изображение')
+    imshow(image, cmap='gray')
+
+    fig.add_subplot(3, 4, 2)
+    plt.title('Оценка модуля лапласиана 1')
+    imshow(laplasian1, cmap='gray')
+
+    fig.add_subplot(3, 4, 3)
+    plt.title('Гистограмма оценки модуля лапласиана 1')
+    hist1, bins1 = histogram(laplasian1)
+    plt.plot(bins1, hist1)
+
+    fig.add_subplot(3, 4, 4)
+    plt.title('Контуры метода аппроксимации лапласиана 1')
+    laplacian_approximation_image1 = thresholding(laplasian1, 85)
+    imshow(laplacian_approximation_image1, cmap='gray')
+
+    fig.add_subplot(3, 4, 6)
+    plt.title('Оценка модуля лапласиана 2')
+    imshow(laplasian2, cmap='gray')
+
+    fig.add_subplot(3, 4, 7)
+    hist2, bins2 = histogram(laplasian2)
+    plt.title('Гистограмма оценки модуля лапласиана 2 ')
+    plt.plot(bins2, hist2)
+
+    fig.add_subplot(3, 4, 8)
+    plt.title('Контуры метода аппроксимации лапласиана 2')
+    laplacian_approximation_image2 = thresholding(laplasian2, 45)
+    imshow(laplacian_approximation_image2, cmap='gray')
+
+    fig.add_subplot(3, 4, 10)
+    plt.title('Оценка модуля лапласиана 3')
+    imshow(laplasian3, cmap='gray')
+
+    fig.add_subplot(3, 4, 11)
+    hist3, bins3 = histogram(laplasian3)
+    plt.title('Гистограмма оценки модуля лапласиана 3')
+    plt.plot(bins3, hist3)
+
+    fig.add_subplot(3, 4, 12)
+    plt.title('Контуры метода аппроксимации лапласиана 3')
+    laplacian_approximation_image3 = thresholding(laplasian3, 65)
+    imshow(laplacian_approximation_image3, cmap='gray')
+
+    plt.show()
+
+    ################################################################
+    s1, s2, e = pruitt_operator(image)
+
+    hist, bins = histogram(e)
+    pruitt_operator_image = thresholding(e, 20)
+
+    fig = plt.figure(figsize=(12, 7))
+    fig.suptitle('Метод оператора Прюитт')
+    fig.add_subplot(2, 3, 1)
+    plt.title('Исходное изображение')
+    imshow(image, cmap='gray')
+
+    fig.add_subplot(2, 3, 2)
+    plt.title('Частная производная по первому направлению')
+    imshow(s1 + 128, cmap='gray')
+
+    fig.add_subplot(2, 3, 3)
+    plt.title('Частная производная по второму направлению')
+    imshow(s2 + 128, cmap='gray')
+
+    fig.add_subplot(2, 3, 4)
+    plt.title('Оценка модуля градиента')
+    imshow(e, cmap='gray')
+
+    fig.add_subplot(2, 3, 5)
+    plt.title('Гистограмма оценки модуля градиента')
+    plt.plot(bins, hist)
+
+    fig.add_subplot(2, 3, 6)
+    plt.title('Контуры метода Прюитт')
+    imshow(pruitt_operator_image, cmap='gray')
+
+    plt.show()
+
+    ################################################################
+    agreement_laplacian_result = agreement_laplacian(image)
+    agreement_laplacian_image = thresholding(agreement_laplacian_result, 35)
+
+    fig = plt.figure(figsize=(12, 7))
+    fig.suptitle('Метод согласования для лапласиана')
+    fig.add_subplot(2, 2, 1)
+    plt.title('Исходное изображение')
+    imshow(image, cmap='gray')
+
+    fig.add_subplot(2, 2, 2)
+    plt.title('Оценка модуля лапласиана')
+    imshow(e, cmap='gray')
+
+    fig.add_subplot(2, 2, 3)
+    plt.title('Гистограмма оценки модуля лапласиана')
+    plt.plot(bins, hist)
+
+    fig.add_subplot(2, 2, 4)
+    plt.title('Контуры метода аппроксимации лапласиана')
+    imshow(agreement_laplacian_image, cmap='gray')
+
+    plt.show()
